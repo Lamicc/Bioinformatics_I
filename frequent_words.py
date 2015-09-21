32befffc4d411796745473d5f0eff55b0d2a4dec
Lamic Chan <lamicccc@gmail.com>
2015-09-21 19:13:24 +0800

    delete redundancy

--- frequent_words.py
+++ frequent_words.py
@@ -109,3 +109,4 @@ def frequent_words_with_mismatches(text, k, d):
 
 
 
+
--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-#from pattern_count import patterncount
-from frequent_words import  most_frequent, frequentwords, fast_most_frequent, fast_frequentwords, find_most_fword_by_sorting, most_app_frequent
+from pattern_count import patterncount
+from frequent_words import  most_frequent, frequentwords, \
+fast_most_frequent, fast_frequentwords, find_most_fword_by_sorting, frequent_words_with_mismatches
 from reverse_complement import findreverse
 from occurrence import find_pattern
-#from P_to_N import pattern_to_number, number_to_pattern, p_to_n_modify, n_to_p_modify
+from P_to_N import pattern_to_number, number_to_pattern, p_to_n_modify, n_to_p_modify
 from computing_frequencies import computingfrequencies
 from find_oric import plot_skew, find_min_skew
 from hamming_distance import compute_hd,approximate_pattern_matching
